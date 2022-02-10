537a6c0043c2566b62527657d672eb26903abfd8
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-10 09:31:56 +0100

    Button generating different data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  const fetchingData = async () => {
+    const response = await fetch(
+      "https://uselessfacts.jsph.pl/random.json?language=en"
+    );
+    const data = await response.json();
+
+    console.log(data);
+    setData(data);
+    setDataLoaded(true);
+  };
+
+  useEffect(() => {
+    fetchingData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!dataLoaded ? (
+        <h1>Loading</h1>
+      ) : (
+        <>
+          <h1>{data.text}</h1>
+          <a href="{data.permalink}" target="_blank">
+            Click here.
+          </a>
+          <button onClick={fetchingData}>Get another fact.</button>
+        </>
+      )}
+    </>
   );
 }
 
